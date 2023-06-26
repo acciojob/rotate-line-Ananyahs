00983f53e1e477c0b49925f8c165fc19b74b4971
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 11:30:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-//your JS code here. If required.
-//your JS code here. If required.
-const c=document.getElementByIf("line");
-const contex=c.getContext("2d");
+#line {
+  width: 200px;
+  height: 2px;
+  background-color: black;
+  animation: rotateLine 2s infinite linear;
+}
+
+@keyframes rotateLine {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
